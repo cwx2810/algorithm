f074c431c1ee2a808c5f918849cbe19af6a8806d
cwx2810 <cwx2810@163.com>
2017-05-03 10:52:18 +0800

    2sum-2

--- 2sum.cpp
+++ 2sum.cpp
@@ -1,33 +1,38 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int> &numbers, int target) {
-        vector<int> ret;
-        if(numbers.size() <= 1)
-            return ret;
-        unordered_map<int,int> myMap;
-        for(int i = 0; i < numbers.size(); ++i)
-            myMap[numbers[i]] = i;
-        for(int i = 0; i < numbers.size(); ++i)
-        {
-            int rest_val = target - numbers[i];
-            if(myMap.find(rest_val)!=myMap.end())
-            {
-                int index = myMap[rest_val];
-                if(index == i)
-                    continue; 
-                if(index < i)
-                {
-                    ret.push_back(index+1); 
-                    ret.push_back(i+1);
-                    return ret;
-                }
-                else
-                {
-                    ret.push_back(i+1);
-                    ret.push_back(index+1);
-                    return ret;
-                }
-            }
-        }
-    }
-};
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+vector<int> twoSum(vector<int> &numbers, int target);
+
+int main(){
+	int a[]={1,2,5,7,9};
+    vector<int> A(a,a+5);
+	int t=7;
+	vector<int> ret=twoSum(A,t);
+	for(int i=0;i<ret.size();i++){
+		cout<<ret[i]<<" ";
+	}
+}
+
+
+
+vector<int> twoSum(vector<int> &numbers, int target) {
+	vector<int> ret;//新建容器保存返回数组
+	unordered_map <int,int> hash;//新建hashmap，key为数组元素，value为数组下标
+	if(numbers.size()<1){
+		return ret;
+	}
+	for(int i=0;i<numbers.size();i++){
+		int numberToFind=target-numbers[i];//遍历数组元素，已知一个求另一个
+		if(hash.find(numberToFind)!=hash.end()){//找到了，把value和i压入vector 
+			ret.push_back(hash[numberToFind]);
+			ret.push_back(i);
+		} else{
+			hash[numbers[i]]=i;//没找到，把键值对存入map 
+		}
+	}
+	return ret;
+	 
+}
+
