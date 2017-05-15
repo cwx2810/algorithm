13321d6d1de468c700a9909d1b4f34b98b82d6c2
cwx2810 <cwx2810@163.com>
2017-05-15 08:31:59 +0800

    missingNumber

--- powerof2.cpp
+++ powerof2.cpp
@@ -18,17 +18,6 @@ int main(){
 //找到顺序数组中丢失的元素
 //比较快的方法是用正确的连续数组的元素和与给定的数组元素和相比较，不同则是丢失的元素
 //利用cpu的性质异或XOR运算比较快，采用异或的方法可以达到On的时间复杂度，O1的空间复杂度 
-int missingNumber(vector<int>& nums){
-	//初始化x
-	int x=0;
-	//异或正确数组
-	for(int i=0;i<=nums.size();i++) {
-		x^=i;
-	}
-	//异或给定数组
-	for(auto n:nums){
-		x^=n;
-	}
-	//返回x 
-	return x;
+bool isPowerOfTwo(int n){
+	
 }
