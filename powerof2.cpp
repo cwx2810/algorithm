f9b26b3d673019f6b235228fb170de83b4d239f5
cwx2810 <cwx2810@163.com>
2017-05-16 08:46:37 +0800

    powerof2

--- powerof2.cpp
+++ powerof2.cpp
@@ -4,20 +4,39 @@
 #include<algorithm>
 using namespace std;
 
-int missingNumber(vector<int>& nums);
+bool isPowerOfTwo(int n);
 
 int main(){
-	int A[]={0,1,2,4,5,6};
-	vector<int> nums(A,A+6);
-	int result=missingNumber(nums);
-	cout<<result;
+	int n=30;
+	bool result=isPowerOfTwo(n);
+	cout<<result; 
 }
 
 
 
-//找到顺序数组中丢失的元素
-//比较快的方法是用正确的连续数组的元素和与给定的数组元素和相比较，不同则是丢失的元素
-//利用cpu的性质异或XOR运算比较快，采用异或的方法可以达到On的时间复杂度，O1的空间复杂度 
+//判断是否给定数是2的幂
+//思路，把给定数转换成二进制，是2的幂则二进制没有1或只有1个1，负数不是2的幂 
 bool isPowerOfTwo(int n){
+	//corner case负数 
+	if(n<0){
+		return false;
+	} 
+	//初始化判断的返回值 
+	bool hasOne=false;
+	//整数位运算 
+	while(n>0){
+		//如果余1则进入判断 
+		if(n&1){
+			//如果hasOne已经真了，则不是2的幂，说明之前算的余数出来1了，有超过1个1 
+			if(hasOne){
+				return false;
+			}else{//否则，1是第一次粗线，则设置hasOne为1 
+				hasOne=true;
+			}
+		}
+		//余0则直接跳过这一位 
+		n>>=1;
+	}
+	return hasOne;
 	
 }
