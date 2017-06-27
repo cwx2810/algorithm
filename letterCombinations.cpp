5fe3f689e4192800766f46d7474c09a94fbf521a
cwx2810 <cwx2810@163.com>
2017-06-27 17:11:51 +0800

    rubbsh

---
+++ letterCombinations.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<algorithm>
+#include<limits>
+using namespace std;
+
+int main(){
+
+}
+
+//有一个电话键盘，随机抽取上面一些数字，要求给出抽取出的数字里所有的字母组合
+//建立dic，对于每一个数字，在dic中查找它对应的字母 
+ 
+
+vector<string> letterCombinations(string digits) {
+    //初始化返回数组
+    vector<vector<int> > ret;
+	//初始化dic
+	//初始化返回数组中每个字母组合 
+	//下标代表0~9，压入所有的字母组合给每一个下标
+	//调用组合函数
+	//返回 
+}
+//构建组合函数，传入返回数组，数组中每个字母组合，给定数字，字典，遍历深度
+//遍历完所有给定数字（深度等于给定数字长度），压栈返回
+//初始化字典查找下标为给定字符串转int
+//迭代相压栈应字典下标内查找到的所有字母
+//字母压栈
+//深度递归
+//递归超过给定数字长度，返回到这里，弹栈 
+ 
+
+ 
+ 
