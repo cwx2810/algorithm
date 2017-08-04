2f5e7e24498babcae85b490ddd2d3cfa907aa32a
cwx2810 <cwx2810@163.com>
2017-08-04 08:20:07 +0800

    补充2sum注释

--- 2sum.cpp
+++ 2sum.cpp
@@ -16,23 +16,33 @@ int main(){
 }
 
 
-
+//给定一个数组和一个值，要求输出此数组的两个下标，这俩下标的值加起来是给定值 
 vector<int> twoSum(vector<int> &numbers, int target) {
-	vector<int> ret;//新建容器保存返回数组
-	unordered_map <int,int> hash;//新建hashmap，key为数组元素，value为数组下标
+	//初始化容器保存返回数组
+	vector<int> ret;
+	//初始化hashmap，key为数组元素，value为数组下标
+	unordered_map <int,int> hash;
+	//corner case 给定数组为空 
 	if(numbers.size()<1){
 		return ret;
 	}
+	//遍历数组元素下标 
 	for(int i=0;i<numbers.size();i++){
-		int numberToFind=target-numbers[i];//遍历数组元素，已知一个求另一个
-		if(hash.find(numberToFind)!=hash.end()){//找到了，把value和i压入vector 
+		//已知一个值可以表示出另一个值，这样每次遍历元素后找到应该匹配的值，在hashmap中找，找到说明满足 
+		int numberToFind=target-numbers[i];
+		//没有一直走到hashmap的最后，说明找到了，把找到的下标value和遍历的下标i压入vector
+		if(hash.find(numberToFind)!=hash.end()){ 
 			ret.push_back(hash[numberToFind]);
 			ret.push_back(i);
 		} else{
-			hash[numbers[i]]=i;//没找到，把键值对存入map 
+			//没找到，把映射存入map
+			//这里要倒着想，一开始map中肯定是没有映射的，找不到的，渐渐存入，才会在遍历到某一元素时找到
+			//刚好上面的压栈顺序也是倒着的，因为遍历到的i在后面是大下标，找到的是已经保存的在前面小下标
+			//仔细揣摩，真是一个节省时间空间的做法，甚至都不在一开始初始化好所有的hashmap，而是初始化映射到刚好够用 
+			hash[numbers[i]]=i; 
 		}
 	}
+	//返回 
 	return ret;
-	 
 }
 
