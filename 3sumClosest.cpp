eb651ed22fb7131a709f46005261a1d4cd850a39
cwx2810 <cwx2810@163.com>
2017-08-06 08:14:50 +0800

    3sumCloset补充注释

--- 3sumClosest.cpp
+++ 3sumClosest.cpp
@@ -15,13 +15,14 @@ int main(){
 }
 
 
-
+//给定数组和一个值，要求找出三个数，他们的和尽量接近给定数 
 //比3sum多了abs判断 
 int threeSumClosest(vector<int>& nums, int target) {
+	//corner case 给定数组不足三个数 
     if(nums.size()<=2){
     	return 0;
 	}
-	int closet=nums[1]+nums[2]+nums[3];//初始化closet 
+	int closet=nums[1]+nums[2]+nums[3];//初始化返回的最接近的三个数只和，初始为前三个数 
 	sort(nums.begin(),nums.end());//先排序 
 	for(int i=0;i<nums.size();i++){
 		int j=i+1;
@@ -34,7 +35,7 @@ int threeSumClosest(vector<int>& nums, int target) {
 			if(abs(target-sum)<abs(target-closet)){//逼近的够小就替换 
 				closet=sum;
 			}
-			if(sum>target){//不满足条件，两端游标不断逼近 
+			if(sum>target){//因为是排过序的，所以发生和比给定数大的情况，一定是太靠外了，不满足条件，两端游标不断逼近 
 				k--;
 			}else{
 				j++;
