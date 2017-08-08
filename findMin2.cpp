c9673a0ac2369815bf47df0d2532819e948d7b68
cwx2810 <cwx2810@163.com>
2017-08-09 07:57:38 +0800

    findMin2补充注释

--- findMin2.cpp
+++ findMin2.cpp
@@ -15,8 +15,9 @@ int main(){
 
 
 
-//有重复元素的数组里找最小值，和没重复的区别就在于如果发现mid=i，跳过i（i++）即可  
+//有重复元素的先排好序又滚动了一次的数组里找最小值，和没重复的区别就在于如果发现mid=开始元素，跳过开始（i++）即可  
 int findMin(vector<int> &nums){
+	//corner case 给定数组仅有0个，1个，2个元素 
 	if(nums.size()==0){
 		return 0;
 	}
@@ -29,15 +30,15 @@ int findMin(vector<int> &nums){
 	int i=0;
 	int j=nums.size()-1;
 	while(i<j-1){
-		if(nums[i]<nums[j]){
+		if(nums[i]<nums[j]){//直接是排好序的，开始元素就是最小的 
 			return nums[i];
 		}
-		int mid=(i+j)/2;
-		if(nums[mid]>nums[i]){
+		int mid=(i+j)/2;//初始化中间点 
+		if(nums[mid]>nums[i]){//中间大于开始，在后面查找 
 			i=mid;
-		}else if(nums[mid]<nums[i]){
+		}else if(nums[mid]<nums[i]){//中间小于开始，在前面查找 
 			j=mid;
-		}else{
+		}else{//中间等于开始，跳过开始 
 			i++;
 		}
 	}
